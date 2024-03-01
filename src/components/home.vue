ca25e8c92aca71ddd7e901f6dd13c8fc3411e682
Shubham <shubhpatil34317@gmail.com>
2024-03-01 14:02:18 +0530

    commit first

---
+++ src/components/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>hello from home</h1>
+    <h1> {{  data }}</h1>
+</template>
+<script>
+
+export default{
+    name : 'home',
+    props:
+    {
+        data : String
+    }
+};
+</script>
